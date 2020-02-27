d21da528ff986ba0ae449d87e45f62a25ef1eb13
Quentin DELAGE <quentin.delage1@etu.unilim.fr>
2020-02-27 11:16:32 +0100

    Mock de réponse
    Répondre "Jonathan" pour faire afficher "BRAVO !"

--- app/src/main/java/fr/unilim/iut/enigmagame/MainActivity.java
+++ app/src/main/java/fr/unilim/iut/enigmagame/MainActivity.java
@@ -3,12 +3,32 @@ package fr.unilim.iut.enigmagame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button bouton;
+    TextView question;
+    EditText saisie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bouton = (Button) findViewById(R.id.bouton);
+        question = (TextView) findViewById(R.id.question);
+        saisie = (EditText) findViewById(R.id.saisie);
+
+        bouton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if ("Jonathan".equals(saisie.getText().toString())){
+                    question.setText("BRAVO !");
+                }
+            }
+        });
+
     }
 }
